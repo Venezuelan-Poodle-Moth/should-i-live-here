2f7b9302007c7270c426ffcb530240873a7bbe70
Mitchel Severe <truth531@gmail.com>
2020-01-26 10:32:52 -0500

    add redux-thunk dependency

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-const db = require('./models/db')
+const db = require('./models/db');
 const userTable = require('./models/userModel');
 
 
 const app = express();
-//parse incoming json
+// parse incoming json
 app.use(express.json());
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
@@ -14,12 +14,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
-//create userTable when server starts up if it doesn't yet exist 
+// create userTable when server starts up if it doesn't yet exist
 db.query(userTable, (err, res) => {
   if (err) {
     console.log(err);
   }
-})
+});
 
 app.listen(3000, () => {
   console.log('Port listening on 3000');
