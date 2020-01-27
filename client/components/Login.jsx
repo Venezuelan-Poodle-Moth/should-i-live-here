3cd307d3b6922c3bd2a486e8a27bd7fc323485f6
Nicole Abramowski <nabramow@gmail.com>
2020-01-27 17:16:42 -0500

    Remove console log in Login.jsx

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = (props) => {
-  console.log('made it to Login component!')
   return (
     <form className="auth" onSubmit={ props.onLoginSubmit }>
       <input type="text" placeholder="Enter username" required />
