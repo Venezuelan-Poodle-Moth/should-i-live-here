0609e46a3831e2af25650e71bbb7e423f3cba6eb
Sergiy Alariki <sergiy.alariki.21@gmail.com>
2020-01-28 15:27:17 -0500

    overview

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       },
       {
         test: /\.scss/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
